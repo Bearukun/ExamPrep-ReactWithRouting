66d96753853825859174f4be9e6586fb6a972f26
DjKirkovic <kirkovic@gmail.com>
2017-06-09 13:11:41 +0200

    Update

--- src/Details.js
+++ src/Details.js
@@ -10,28 +10,58 @@ class Details extends React.Component {
     }
     render() {
 
-        var users = this.state.jsonData.users;
-
-        const rows = users.map ((user, index) => {
-            return (
-                <tr key={index}>
-                    <td><img src={user.picture.thumbnail}/></td>
-                    <td>{user.first + " "+ user.last}</td>
-                    <td><Link to={"/details/"+index}>
-                        <button>Details</button>
-
-                    </Link></td>
-                </tr>
-            )
-        });
-
+        var person = this.state.jsonData.users[this.props.params.id];
 
         return (
             <div>
 
-        lwakdnælkdnw
-                {this.props.params.id}
-                abc {rows}
+                <h1>Details for {person.first + " " + person.last}</h1>
+                <table>
+                    <tr>
+                        <td>Gender</td>
+                        <td>{person.gender}</td>
+                    </tr>
+                    <tr>
+                        <td>First name</td>
+                        <td>{person.first}</td>
+                    </tr>
+                    <tr>
+                        <td>Last name</td>
+                        <td>{person.last}</td>
+                    </tr>
+                    <tr>
+                        <td>Street</td>
+                        <td>{person.street}</td>
+                    </tr>
+                    <tr>
+                        <td>City</td>
+                        <td>{person.city}</td>
+                    </tr>
+                    <tr>
+                        <td>Zip code</td>
+                        <td>{person.zip}</td>
+                    </tr>
+                    <tr>
+                        <td>Phone</td>
+                        <td>{person.phone}</td>
+                    </tr>
+                    <tr>
+                        <td>Cell phone</td>
+                        <td>{person.cell}</td>
+                    </tr>
+                    <tr>
+                        <td>Email address</td>
+                        <td>{person.email}</td>
+                    </tr>
+                    <tr>
+                        <td><img src={person.picture.large}/></td>
+                    </tr>
+                </table>
+
+                <Link to={"/list/"}>
+                    <button>Back</button>
+                </Link>
+
             </div>
         )
     }
